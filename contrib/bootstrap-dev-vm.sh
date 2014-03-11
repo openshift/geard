b569c52300bfc4d3b93431d563a8cbb2af958557
Paul Morie <pmorie@gmail.com>
2014-03-11 10:48:49 -0400

    Finishing touches: project fixup

--- contrib/bootstrap-dev-vm.sh
+++ contrib/bootstrap-dev-vm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Installing dependencies for geard development"
+echo "Installing dependencies and setting up vm for geard development"
 
 yum install -y docker-io golang git hg bzr libselinux-devel
 usermod -a -G docker vagrant
@@ -11,11 +11,11 @@ systemctl status docker
 mkdir -p ~vagrant/go/src/github.com/smarterclayton
 chown -R vagrant:vagrant ~vagrant/go
 ln -s /vagrant ~vagrant/go/src/github.com/smarterclayton/geard
-chown -h vagrant:vagrant !$
+chown -h vagrant:vagrant ~vagrant/go/src/github.com/smarterclayton/geard
 
 # Install / enable systemd unit
-ln -s /vagrant/contrib/geard.service /usr/lib/systemd/system/geard.service
-systemctl enable geard.service
+cp -f /vagrant/contrib/geard.service /usr/lib/systemd/system/geard.service
+systemctl enable /usr/lib/systemd/system/geard.service
 
 echo 'export GOPATH=~/go' >> ~vagrant/.bash_profile
 echo 'export PATH=$GOPATH:$PATH' >> ~vagrant/.bash_profile
