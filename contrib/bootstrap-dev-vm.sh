fc7ae797d07deadc4ff809e6cdc9b5fdccfeb9ec
Paul Morie <pmorie@gmail.com>
2014-03-11 02:41:10 -0400

    Handle systemd/docker build

--- contrib/bootstrap-dev-vm.sh
+++ contrib/bootstrap-dev-vm.sh
@@ -7,9 +7,16 @@ systemctl enable docker.service
 systemctl start docker
 systemctl status docker
 
+# Setup local fork to play nice with 'go'
 mkdir -p ~vagrant/go/src/github.com/smarterclayton
 chown -R vagrant:vagrant ~vagrant/go
 ln -s /vagrant ~vagrant/go/src/github.com/smarterclayton/geard
+chown -h vagrant:vagrant !$
+
+# Install / enable systemd unit
+ln -s /vagrant/contrib/geard.service /usr/lib/systemd/system/geard.service
+systemctl enable geard.service
+
 echo 'export GOPATH=~/go' >> ~vagrant/.bash_profile
 echo 'export PATH=$GOPATH:$PATH' >> ~vagrant/.bash_profile
 echo 'cd /vagrant' >> ~vagrant/.bashrc
