71a7ff20a329d8db86e285402d1a6d82ec433c46
Clayton Coleman <ccoleman@redhat.com>
2014-04-03 14:39:11 -0400

    Link constraint ordering wrong, reverse

--- deployment/deployment.go
+++ deployment/deployment.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/openshift/geard/containers"
+	"log"
 	"os"
 	"strconv"
 )
@@ -97,6 +98,8 @@ func (d Deployment) Describe(placement PlacementStrategy) (next *Deployment, rem
 		err = erro
 		return
 	}
+
+	log.Printf("Order: %+v", links)
 
 	// expose ports for all links
 	for i := range links {
