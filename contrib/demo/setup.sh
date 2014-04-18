2dba81b27cc592cb95a1792b915bee3061544a14
Clayton Coleman <ccoleman@redhat.com>
2014-04-17 21:08:32 -0700

    Add a geard demo
    The provided contrib/demo/setup.sh sets up a pre-environment
    for the demo, although it needs to be refactored to use all
    public images.  Browsing on localhost:14000/scale-1.0/ will
    show the state of the loadbalancer.  Also demonstrates mongo
    replica sets and OpenShift on top of geard.
    
    Run contrib/demo/demo.sh to run the demo, and
    contrib/demo/teardown.sh to delete all running containers at
    the end.  Also, will need to clean the /var/lib/docker/vfs/dir
    directory to ensure any persisted Mongo data is deleted.

---
+++ contrib/demo/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -ex
+
+base=$(dirname $0)
+
+id=$(docker inspect --format="{{.id}}" goldmann/mongod)
+ret=$?
+if [ $ret -ne 0 ]; then
+  docker pull ccoleman/geard-githost
+  docker pull 10.10.188.116/jboss/eap
+  docker tag 10.10.188.116/jboss/eap jboss/eap
+  docker pull 10.10.188.116/ccoleman/eap-scaling-demo
+  docker tag 10.10.188.116/ccoleman/eap-scaling-demo ccoleman/eap-scaling-demo
+  docker pull 10.10.188.116/ccoleman/eap-scaling-demo-new
+  docker tag 10.10.188.116/ccoleman/eap-scaling-demo-new ccoleman/eap-scaling-demo-new
+  docker pull 10.64.27.125:5000/goldmann/mongod
+  docker tag 10.64.27.125:5000/goldmann/mongod goldmann/mongod
+  docker pull pmorie/sti-html-app
+  docker pull ccoleman/ubuntu-mongodb-repl
+  docker pull ccoleman/openshift-broker-docker
+  docker pull dockerfile/mongodb
+  docker tag 103bd59de294 jboss/eap # tag is in the history
+fi
+
+set +ex
+
+$base/teardown.sh
+
+gear deploy $base/deploy_eap_cluster.json localhost
+sleep 3
+gear stop localhost/demo-backend-3
+
+$base/wait_for_url.sh "http://localhost:14000/scale-1.0/"
