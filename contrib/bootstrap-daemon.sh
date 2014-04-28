cb812d5a37a1287025ed906d7bf33e272757037a
derekwaynecarr <decarr@redhat.com>
2014-04-28 10:49:33 -0400

    Cleanup vagrant initialization, default build gear

--- contrib/bootstrap-daemon.sh
+++ contrib/bootstrap-daemon.sh
@@ -2,8 +2,8 @@
 echo "Initializing gear daemon"
 
 echo "To use the geard service, run"
-echo "systemctl enable /usr/lib/systemd/system/geard-image.service"
-echo "systemctl start geard-image.service"
+echo "sudo systemctl enable $GOPATH/src/github.com/openshift/geard/contrib/geard.service"
+echo "sudo systemctl start geard.service"
 echo ""
 echo "To use the geard-githost service, run"
 echo "systemctl enable /var/lib/containers/units/geard-githost.service"
--- contrib/bootstrap-dev-vm.sh
+++ contrib/bootstrap-dev-vm.sh
@@ -13,14 +13,37 @@ GEARD_PATH=/vagrant/src/github.com/openshift/geard
 chown -R vagrant:vagrant /vagrant
 
 # Modify SSHD config to use gear-auth-keys-command to support git clone from repo
-echo 'AuthorizedKeysCommand /usr/sbin/gear-auth-keys-command' >> /etc/ssh/sshd_config
-echo 'AuthorizedKeysCommandUser nobody' >> /etc/ssh/sshd_config
+if [[ $(cat /etc/ssh/sshd_config | grep /gear-auth-keys-command) = "" ]]; then
+  echo 'AuthorizedKeysCommand /usr/sbin/gear-auth-keys-command' >> /etc/ssh/sshd_config
+  echo 'AuthorizedKeysCommandUser nobody' >> /etc/ssh/sshd_config
+else
+  echo "AuthorizedKeysCommand already configured"
+fi
 
-echo 'export GOPATH=/vagrant' >> ~vagrant/.bash_profile
-echo 'export PATH=$GOPATH/bin:$PATH' >> ~vagrant/.bash_profile
-echo "cd $GEARD_PATH" >> ~vagrant/.bashrc
-echo "bind '\"\e[A\":history-search-backward'" >> ~vagrant/.bashrc
-echo "bind '\"\e[B\":history-search-forward'" >> ~vagrant/.bashrc
+# SET VAGRANT USER PATH VARIABLES: GOPATH, GEARD_PATH
+if [[ $(cat ~vagrant/.bash_profile | grep GOPATH) = "" ]]; then
+  echo 'export GOPATH=/vagrant' >> ~vagrant/.bash_profile
+  echo 'export PATH=$GOPATH/bin:$PATH' >> ~vagrant/.bash_profile
+  echo "cd $GEARD_PATH" >> ~vagrant/.bashrc  
+  echo "bind '\"\e[A\":history-search-backward'" >> ~vagrant/.bashrc
+  echo "bind '\"\e[B\":history-search-forward'" >> ~vagrant/.bashrc
+else
+  echo "vagrant user path variables already configured"
+fi
+
+# SET ROOT USER PATH VARIABLES: GOPATH, GEARD_PATH
+if [[ $(cat /root/.bash_profile | grep GOPATH) = "" ]]; then
+  echo 'export GOPATH=/vagrant' >> /root/.bash_profile
+  echo 'export PATH=$GOPATH/bin:$PATH' >> /root/.bash_profile
+  echo "cd $GEARD_PATH" >> /root/.bashrc
+  echo "bind '\"\e[A\":history-search-backward'" >> /root/.bashrc
+  echo "bind '\"\e[B\":history-search-forward'" >> /root/.bashrc  
+else
+  echo "root user path variables already configured"
+fi
+
+echo "Performing initial geard build..."
+su --login --shell="/bin/bash" --session-command "cd $GEARD_PATH && contrib/build" vagrant
 
 echo "Docker access will be enabled when you 'vagrant ssh' in."
 echo "Run 'contrib/build' to build your source."
