832a236f897d867485c07c7037cc13933fd84201
Paul Morie <pmorie@gmail.com>
2014-04-29 14:59:36 -0400

    Updates for newest go-dockerclient version

--- cmd/gear/commands.go
+++ cmd/gear/commands.go
@@ -131,6 +131,7 @@ func Execute() {
 	buildCmd.Flags().StringVar(&(buildReq.WorkingDir), "dir", "tempdir", "Directory where generated Dockerfiles and other support scripts are created")
 	buildCmd.Flags().StringVarP(&(buildReq.RuntimeImage), "runtime", "R", "", "Set the runtime image to use")
 	buildCmd.Flags().StringVarP(&(buildReq.Method), "method", "m", "run", "Specify a method to build with. build -> 'docker build', run -> 'docker run'")
+	buildCmd.Flags().StringVarP(&(buildReq.Ref), "ref", "r", "", "Specify a ref to check-out")
 	buildCmd.Flags().BoolVar(&(buildReq.Debug), "debug", false, "Enable debugging output")
 	buildCmd.Flags().StringVar(&environment.Path, "env-file", "", "Path to an environment file to load")
 	buildCmd.Flags().StringVar(&environment.Description.Source, "env-url", "", "A url to download environment files from")
