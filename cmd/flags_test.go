a9b861bcc7caf1742c6c917e5712439390beece0
Matt Hicks <matthicksj@gmail.com>
2014-05-20 10:23:43 -0400

    Don't force quoting on environment write.
    Docker's environment file will escape any quotes that exist, so
    make sure not to quote environment values when writing them out.
    Instead, just use line separation.
    
    If you want quoted values in your environment file, you can double
    quote the values and only the first wrapper will be removed.

--- cmd/flags_test.go
+++ cmd/flags_test.go
@@ -11,3 +11,24 @@ func TestGenerateId(t *testing.T) {
 		t.Error("Expected generated ID to be non empty")
 	}
 }
+
+func TestExtractVariablesFrom_NoAppend(t *testing.T) {
+	args := []string{"arg1", "a=b", "arg2", "c=d"}
+
+	env := EnvironmentDescription{}
+
+	if err := env.ExtractVariablesFrom(args, false); err != nil {
+		t.Error("Unexpected error parsing arguments")
+	}
+
+	if len(env.Description.Variables) != 2 {
+		t.Error("Too few parsed arguments")
+	}
+
+	if env.Description.Variables[0].Name != "a" &&
+		env.Description.Variables[0].Value != "b" &&
+		env.Description.Variables[1].Name != "c" &&
+		env.Description.Variables[1].Value != "d" {
+		t.Error("Incorrect argument parsing")
+	}
+}
