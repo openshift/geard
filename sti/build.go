0066ee2bb481f5c53a6ad6a8ecfa0a76c6c6ee5e
jhadvig <jhadvig@redhat.com>
2014-05-27 17:12:13 +0200

    Running save artifact command as a user

--- sti/build.go
+++ sti/build.go
@@ -115,7 +115,7 @@ chown -R {{.User}}:{{.User}} /tmp/artifacts && chmod -R 755 /tmp/artifacts
 chown -R {{.User}}:{{.User}} /tmp/scripts && chmod -R 755 /tmp/scripts
 chown -R {{.User}}:{{.User}} /tmp/defaultScripts && chmod -R 755 /tmp/defaultScripts
 chown -R {{.User}}:{{.User}} /tmp/src && chmod -R 755 /tmp/src
-exec su {{.User}} -s /bin/sh -c {{.SaveArtifactsPath}}
+exec su {{.User}} - -s /bin/sh -c {{.SaveArtifactsPath}}
 `))
 
 // Script used to initialize permissions on bind-mounts for a docker-run build (prepare call)
@@ -131,9 +131,9 @@ fi
 
 if [ -f {{.AssemblePath}} ]; then
 	{{if .Usage}}
-		exec su {{.User}} -s /bin/sh -c "{{.AssemblePath}} -h"
+		exec su {{.User}} - -s /bin/sh -c "{{.AssemblePath}} -h"
 	{{else}}
-		exec su {{.User}} -s /bin/sh -c {{.AssemblePath}}
+		exec su {{.User}} - -s /bin/sh -c {{.AssemblePath}}
 	{{end}}
 else
   echo "No assemble script supplied in ScriptsUrl argument, application source, or default url in the image."
