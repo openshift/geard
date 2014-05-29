cd92efe535470c5b14516d3e5572ebc90a17cc32
derekwaynecarr <decarr@redhat.com>
2014-05-29 16:34:49 -0400

    Gear deploy uses a simple in-order placement strategy

--- contrib/demo/setup-multi.sh
+++ contrib/demo/setup-multi.sh
@@ -33,9 +33,10 @@ $base/teardown-multi.sh
 
 descriptor=$base/deploy_parks_map_multihost.json
 
-gear deploy $descriptor localhost 192.168.205.11
+gear deploy $descriptor localhost localhost 192.168.205.11 192.168.205.11 localhost
 gear stop 192.168.205.11/parks-backend-{2,3}
 
 $base/wait_for_url.sh "http://localhost:14000/"
+
 
 sudo switchns --container=parks-db-1 -- /bin/bash -c "curl https://raw.githubusercontent.com/thesteve0/fluentwebmap/master/parkcoord.json | mongoimport -d fluent -c parkpoints --type json && mongo fluent --eval 'db.parkpoints.ensureIndex( { pos : \"2d\" } );'"
